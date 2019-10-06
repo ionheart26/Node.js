c99e2f759b5b327f1599aa6fdbc1caa400952abe
ionheart26 <36890032+ionheart26@users.noreply.github.com>
2019-10-06 22:05:21 +0900

    Create getAndDumpFromDB.js

---
+++ mysql/getAndDumpFromDB.js
@@ -0,0 +1,106 @@
+/*
+ * GET으로 호출하는 경우.
+ * http://localhost:8080/log?device=202&unit=3&type=T&value=24.2&seq=34
+*/
+
+var express = require('express');
+var app = express();
+
+mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'mypassword',
+    database: 'mydb'
+})
+connection.connect();
+
+function insert_sensor(device, unit, type, value, seq, ip) {
+  obj = {};
+  obj.seq = seq;
+  obj.device = device;
+  obj.unit = unit;
+  obj.type = type;
+  obj.value = value;
+  obj.ip = ip.replace(/^.*:/, '')
+
+  var query = connection.query('insert into sensors set ?', obj, function(err, rows, cols) {
+    if (err) throw err;
+    console.log("database insertion ok= %j", obj);
+  });
+}
+
+app.get('/', function(req, res) {
+  res.end('Nice to meet you');
+});
+
+app.get('/log', function(req, res) {
+  r = req.query;
+  console.log("GET %j", r);
+
+  insert_sensor(r.device, r.unit, r.type, r.value, r.seq, req.connection.remoteAddress);
+  res.end('OK:' + JSON.stringify(req.query));
+});
+
+app.get("/dump", function(req, res) {
+  console.log("param=" + req.query);
+
+  var qstr = 'select * from sensors ';
+  connection.query(qstr, function(err, rows, cols) {
+    if (err) {
+      throw err;
+      res.send('query error: '+ qstr);
+      return;
+    }
+
+    console.log("Got "+ rows.length +" records");
+    html = ""
+    for (var i=0; i< rows.length; i++) {
+       html += JSON.stringify(rows[i]);
+       html += "<br/>"
+    }
+    res.send(html);
+  });
+
+});
+
+app.get('/graph', function (req, res) {
+    console.log('got app.get(graph)');
+    var html = fs.readFile('./graph.html', function (err, html) {
+    html = " "+ html
+    console.log('read file');
+
+    var qstr = 'select * from sensors ';
+    connection.query(qstr, function(err, rows, cols) {
+      if (err) throw err;
+
+      var data = "";
+      var comma = ""
+      for (var i=0; i< rows.length; i++) {
+         r = rows[i];
+         data += comma + "[new Date(2017,04-1,"+ r.id +",00,38),"+ r.value +"]";
+         comma = ",";
+      }
+      var header = "data.addColumn('date', 'Date/Time');"
+      header += "data.addColumn('number', 'Temp');"
+      html = html.replace("<%HEADER%>", header);
+      html = html.replace("<%DATA%>", data);
+
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.end();
+    });
+  });
+})
+
+var server = app.listen(8080, function () {
+  var host = server.address().address
+//  var port = server.address().port
+  console.log('listening at http://%s:%s', host, '8080')
+});
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+//  var port = server.address().port
+  console.log('listening at http://%s:%s', host, '8081')
+});
